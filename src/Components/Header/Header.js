b6eee3f6fb1e0f062116b9febc708bb69d8b0d5e
Katie Caserta <katiecaserta@gmail.com>
2022-08-03 22:53:34 +0100

    fix font sizes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,23 +1,29 @@
 import "./Header.scss";
 import lighthouseLogo from "../../assets/images/lighthouseLogo.png";
+import { Link } from "react-router-dom";
 
 import React from "react";
 
 const Header = () => {
   return (
     <div className="header">
-      <div className="header__logo-container">
-        <div className="header__logo-icon-wrapper">
-          <img
-            src={lighthouseLogo}
-            alt="lighthouse icon"
-            className="header__logo"
-          />
+      <Link to="/">
+        <div className="header__logo-container">
+          <div className="header__logo-icon-wrapper">
+            <img
+              src={lighthouseLogo}
+              alt="lighthouse icon"
+              className="header__logo"
+            />
+          </div>
+          <p className="header__logo-text">Lighthouse</p>
         </div>
-        <p className="header__logo-text">Lighthouse</p>
-      </div>
+      </Link>
       <ul className="header__nav-list">
-        <li className="header__nav-list-item">Find a service</li>
+        <li className="header__nav-list-item">
+          <Link to="/">Find a service</Link>
+        </li>
+
         <li className="header__nav-list-item">Progress</li>
         <li className="header__nav-list-item">About</li>
       </ul>
